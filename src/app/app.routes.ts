89b5708bf00305f57ec92e72ec884ee17a562346
Teguh Bagas Mardiansyah <116806781+TeguhBagasM@users.noreply.github.com>
2025-09-15 00:07:58 +0300

    refactor: comment out notes-related code in routing and dashboard components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { TodoComponent } from './todo/todo';
-import { NoteComponent } from './note/note';
+// import { NoteComponent } from './note/note';
 import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
   { path: '', component: Dashboard },
   { path: 'todo', component: TodoComponent },
-  { path: 'notes', component: NoteComponent },
+  // { path: 'notes', component: NoteComponent },
 ];
